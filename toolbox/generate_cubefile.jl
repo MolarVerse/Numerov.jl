d0c83bc0aba5561af74be9654af4acf2d02873ca
97gamjak <jakob.gamper@student.uibk.ac.at>
2022-12-05 08:22:23 +0100

    cube file generator finished

--- toolbox/generate_cubefile.jl
+++ toolbox/generate_cubefile.jl
@@ -1,11 +1,37 @@
 using DelimitedFiles
+using Printf
 
-function generate_cubefile(filename::String)
+function generate_cubefile(filename::String, x::Int, y::Int, z::Int; outputfilename="mycube.cube", column=4)
     data = readdlm(filename)
 
-    x = 63
-    y = 61
-    z = 53
+    step = data[2,3] - data[1,3]
 
-    println("my cube file")
+    file = open(outputfilename, "w")
+
+    println(file, "my cube file")
+    println(file, "###")
+    println(file, "1        0.0         0.0         0.0")
+    println(file, x, "     ", step, "   0.0    0.0")
+    println(file, y, "      0.0       ", step, "    0.0")
+    println(file, z, "      0.0         0.0      ", step)
+    println(file, "1 0.0 0.0 0.0")
+
+    count = 0
+    for i in 1:size(data)[1]
+
+        count += 1
+
+        @printf(file, "%.6e ", data[i,column])
+
+        if count % 6 == 0 || count % z == 0
+            println(file)
+        end
+
+        if count % z == 0
+            count = 0
+        end
+    end
+
+    close(file)
+        
 end
