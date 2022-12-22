b0d3cf191e585ac03249f02019f29faa2330b4a1
97gamjak <jakob.gamper@student.uibk.ac.at>
2022-12-22 17:34:17 +0100

    periodic information added in a crude form

--- src/printLogFile/systemInfo.jl
+++ src/printLogFile/systemInfo.jl
@@ -6,6 +6,18 @@ function systemInfo(files::Files, potential::Potential, system::System)
     massUnit      = potential.massUnit
     potentialUnit = potential.potentialUnit
 
+    a = map(x -> x ? "x" : "-", potential.periodic)
+
+    while length(a) < 3
+        push!(a, " ")
+    end
+
+    b = map(x -> x && potential.reciprocal ? "x" : "-", potential.periodic) #TODO: make reciprocal into all periodic directions
+
+    while length(b) < 3
+        push!(b, " ")
+    end
+
     stringbuffer = 
     "                                                                                        " * "\n" *
     "       ------------------------------------------------------------------------------   " * "\n" *
@@ -17,11 +29,13 @@ function systemInfo(files::Files, potential::Potential, system::System)
     "         coords unit    = $(coordsUnit)                                                 " * "\n" *
     "         mass unit      = $(massUnit)                                                   " * "\n" *
     "         potential unit = $(potentialUnit)                                              " * "\n" *
+    "                                                                                        " * "\n" *
+    "                    |   x   |   y   |   z   |                                           " * "\n" *
+    "                    -------------------------                                           " * "\n" *
+    "         periodic   |   $(a[1])   |   $(a[2])   |   $(a[3])   |                         " * "\n" *
+    "         reciprocal |   $(b[1])   |   $(b[2])   |   $(b[3])   |                         " * "\n" *
+    "         n datapoints |   $(b[1])   |   $(b[2])   |   $(b[3])   |                         " * "\n" * #TODO: make this table dynamic!!!
     "                                                                                        " * "\n"
-
-    tmpbuffer = ""
-
-    #TODO_
 
     print(files.logFile, stringbuffer)
 end
